0ea85f4ec24a7b433b43f0633608fdb27d5bf119
Ray <wrfries81@hotmail.com>
2024-09-24 20:36:17 -0500

    Started JavaScript functions sorting etc.

--- books.js
+++ books.js
@@ -1,3 +1,61 @@
+function renderBooks(filter) {
+  const booksWrapper = document.querySelector(".books");
+
+  const books = getBooks();
+  
+  if (filter === "LOW_TO_HIGH") {
+    books.sort((a, b) => a.originalPrice - b.originalPrice);
+  }
+  else if (filter === "HIGH_TO_LOW") {
+    books.sort((a, b) => b.originalPrice - a.originalPrice);
+  }
+  else if (filter === "RATING") {
+    books.sort((a, b) => b.rating - a.rating);
+  }
+
+  let ratingHTML = "";
+  let rating = 4.5;
+
+  for (let i = 0; i < Math.floor(rating); i++) {
+    ratingHTML += '<i class="fas fa-star"></i>\n';
+  }
+
+  if (Number.isInteger(rating))
+
+  console.log(ratingHTML)
+  const booksHtml = books.map(book => { 
+  return`<div class="book">
+    <figure class="book__img--wrapper">
+      <img class="book__img" src="${book.url}" alt="">
+    </figure>
+    <div class="book__title">
+      ${book.title}
+    </div>
+    <div class="book__ratings">
+      <i class="fas fa-star"></i>
+      <i class="fas fa-star"></i>
+      <i class="fas fa-star"></i>
+      <i class="fas fa-star"></i>
+      <i class="fas fa-star-half-alt"></i>
+    </div>
+    <div class="book__price">
+      <span>$${book.originalPrice.toFixed(2)}</span>
+    </div>
+  </div>`
+  }).join("");
+  
+  booksWrapper.innerHTML = booksHtml;
+}
+
+function filterBooks(event) {
+  renderBooks(event.target.value);
+}
+
+
+setTimeout(() => {
+  renderBooks();
+})
+
 
 
 // FAKE DATA
