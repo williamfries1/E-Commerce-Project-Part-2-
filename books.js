70d32ea68db02f42585c51f6c824da1797eb6ccb
Ray <wrfries81@hotmail.com>
2024-09-24 20:47:10 -0500

    Created new rating functions

--- books.js
+++ books.js
@@ -12,17 +12,7 @@ function renderBooks(filter) {
   else if (filter === "RATING") {
     books.sort((a, b) => b.rating - a.rating);
   }
-
-  let ratingHTML = "";
-  let rating = 4.5;
-
-  for (let i = 0; i < Math.floor(rating); i++) {
-    ratingHTML += '<i class="fas fa-star"></i>\n';
-  }
-
-  if (Number.isInteger(rating))
-
-  console.log(ratingHTML)
+  
   const booksHtml = books.map(book => { 
   return`<div class="book">
     <figure class="book__img--wrapper">
@@ -32,11 +22,7 @@ function renderBooks(filter) {
       ${book.title}
     </div>
     <div class="book__ratings">
-      <i class="fas fa-star"></i>
-      <i class="fas fa-star"></i>
-      <i class="fas fa-star"></i>
-      <i class="fas fa-star"></i>
-      <i class="fas fa-star-half-alt"></i>
+      ${ratingsHTML(book.rating)}
     </div>
     <div class="book__price">
       <span>$${book.originalPrice.toFixed(2)}</span>
@@ -45,6 +31,17 @@ function renderBooks(filter) {
   }).join("");
   
   booksWrapper.innerHTML = booksHtml;
+}
+
+function ratingsHTML(rating) {
+  let ratingHTML = "";
+  for (let i = 0; i < Math.floor(rating); i++) {
+    ratingHTML += '<i class="fas fa-star"></i>\n';
+  }
+  if (!Number.isInteger(rating)) {
+    ratingHTML += '<i class="fas fa-star-half-alt"></i>\n';
+  }
+  return ratingHTML;
 }
 
 function filterBooks(event) {
